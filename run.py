5fe09b4c2ac0cae788ae4fc00eff0f41ae6f99a1
thiago-NSampaio <110494178+thiago-NSampaio@users.noreply.github.com>
2024-08-06 16:56:57 -0400

    changes for production

--- run.py
+++ run.py
@@ -2,9 +2,13 @@ from src.main.server.server import app
 from src.models.settings.db_connection_handler import DBConnectionHandler
 from src.models.settings.initDB import InitDB
 
-if __name__ == "__main__":
+
+def start_app():
     InitDB.create_tables()
     db_connection_handler = DBConnectionHandler()
     db_connection_handler.connect()
-    db_connection_handler.get_connection()
-    app.run(host="0.0.0.0", port=3555, debug=True)
+
+
+if __name__ == "__main__":
+    start_app()
+    app.run(host="0.0.0.0", port=3333, debug=False)
