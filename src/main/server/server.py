97cd373a554a1a6b9534f64f064bb395fbd8ff7a
thiago-NSampaio <110494178+thiago-NSampaio@users.noreply.github.com>
2024-08-21 15:06:17 -0400

    change in CORS

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from flask import Flask
 from src.main.routes.trips_routes import trips_route_bp
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*",
+     "methods": ["GET", "POST", "PUT", "PATCH", "DELETE"]}})
 
 app.register_blueprint(trips_route_bp)
